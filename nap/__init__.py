1e7736be560e4de74097f2d7c226624dc2be584b
yvesmartindestaillades <yves.mrtn98@gmail.com>
2022-06-01 23:43:01 -0500

    doc for pot, reformatting, renaming

--- nap/__init__.py
+++ nap/__init__.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.abspath(""))
 sys.path.append(f"{os.path.abspath('')}/libs/dreem")
 
 import nap.data_wrangler
-import nap.firebase
+import nap.database
 import nap.plot
 import nap.utils
+
