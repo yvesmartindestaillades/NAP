77425add5d47e26799f880c9427d6e8e87115844
yvesmartindestaillades <yves.mrtn98@gmail.com>
2022-06-12 16:49:12 -0500

    updategitignore

--- make_docs.sh
+++
@@ -1,9 +0,0 @@
-cd docs
-echo ''
-echo '            MAKE  CLEAN              '
-echo '------------------------------------'
-make clean
-echo ''
-echo '            MAKE  HTML              '
-echo '------------------------------------'
-make html 
