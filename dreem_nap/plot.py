cf83a5fca1ba40b744db9db49908314b75c24eda
yvesmartindestaillades <yves.mrtn98@gmail.com>
2022-07-15 12:26:56 -0500

    added plot.mut_rate_vs_base_pairing_prob

--- dreem_nap/plot.py
+++ dreem_nap/plot.py
@@ -124,6 +124,75 @@ def heatmap(df:pd.DataFrame, column:str)->None:
     sns.heatmap(base_cov_plot, annot=False, linewidths=0, ax=ax, norm=LogNorm())
 
 
+
+def mut_rate_vs_base_pairing_prob(df:pd.DataFrame, samp:str, construct:int):
+    """Plot a mutation rate histogram, a base non-pairing probability histogram, and a scatter plot fitting the mutation rate vs the base non-pairing. 
+       The base non-pairing values are 1 - base-pairing values.
+
+    Args:
+        df (pd.DataFrame): your dataset
+        samp (str): your sample of interest
+        construct (int): your construct of interest.
+    """
+    def split_bases(df, column):
+        """Split a sample-construct dataframe into 4 columns, one per base. The columns contain the value of the column 'column' entered as an input.
+        """
+
+        df = pd.DataFrame(df[column])
+        df_hist = pd.DataFrame()
+        df_hist.index = df.reset_index()['index']
+
+        for base in ['A','C','G','T']:
+            df_hist[base] = pd.Series(dtype=float)
+            df_hist[base] = df.loc[base]
+
+        return df_hist
+
+    def plot_histogram(df, title):
+        """Plot histogram using a base-wise splitted sample-construct dataframe.
+        """
+
+        ax = df.plot.bar(stacked=True, figsize=(35,7), color=['r','b','y','g'], rot=90)
+        plt.title(f"{title}, sample {samp}, construct {construct}")
+    
+    def plot_scatter(df):
+        """Plot scatter plot using a base-wise splitted sample-construct dataframe.
+        """
+        
+        from sklearn.linear_model import LinearRegression
+
+        x, y = df['base non-pairing probability'], df['mut_rate']
+        x, y = np.array(x).reshape(-1,1), np.array(y).reshape(-1,1)
+        fig = plt.figure(figsize=(5,5))
+
+        for base, color in zip(['A','C','G','T'], ['r','b','y','g']):
+            px, py = split_bases(df, 'base non-pairing probability'), split_bases(df, 'mut_rate')
+            plt.plot(px[base], py[base],color+'.')
+        plt.legend(['A','C','G','T'])
+        plt.xlabel('Base non-pairing probability')
+        plt.ylabel('Mutation rate')
+
+        model = LinearRegression().fit(x,y)
+        plt.plot(model.predict(np.array([0,1]).reshape(-1,1)))
+        plt.title(f"Base non-pairing prob vs mut rate \n R2= {round(model.score(x, y),3)}, sample {samp}, construct {construct}")    
+
+
+    # Data wrangling
+    sc = df.set_index(['samp','construct']).loc[samp, construct]
+
+    df_sc = data_manip.get_roi_info(df, samp, construct, bases_type=['A','C','G','T'], roi_range=list(range(0,171)))\
+                                    .reset_index()\
+                                    .drop(columns=['paired','roi_structure_comparison','roi_deltaG'])\
+                                    .set_index(['base','index'])
+    df_sc['base non-pairing probability'] = 1 - df_sc['base_pairing_prob']
+
+    # Plots
+    plot_histogram(split_bases(df_sc, 'base non-pairing probability'), 'Base non-pairing probability')
+    plot_histogram(split_bases(df_sc, 'mut_rate'), 'Mutation rate')
+    plot_scatter(df_sc)
+
+
+
 def mut_histogram(plot_type:str, df:pd.DataFrame, samp:str, construct:int)->None:
     """Plot the mutation rate of a specific (sample, construct).
 
